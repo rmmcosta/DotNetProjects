6cf91469661052a6dbfa99978f81ce8675628298
rmmcosta <ricardocosta101085@gmail.com>
2021-02-20 15:39:02 +0000

    thread safe queues and yield return to simulate an endless queue and how to keep processing it if it's not empty

---
+++ QueuesWithYieldReturn/QueuesWithYieldReturn/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace QueuesWithYieldReturn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create a thread safe queue
+            var queue = new ThreadSafeQueue<ulong>();
+            //starting filling the queue in another thread
+            ThreadPool.QueueUserWorkItem(AddItemsToQueue, queue);
+            //start processing elements from the queue as they arrive
+            foreach (var each in ProcessItemsInQueue(queue))
+            {
+                Console.WriteLine(each);
+            }
+
+        }
+
+        private static void AddItemsToQueue(Object state)
+        {
+            var queue = (ThreadSafeQueue<ulong>)state;
+            var random = new Random();
+            ulong count = 0;
+            while (true)
+            {
+                queue.Enqueue(count++);
+                Thread.Sleep(random.Next(100, 1000));
+            }
+        }
+
+        private static IEnumerable<ulong> ProcessItemsInQueue(Object state)
+        {
+            var queue = (ThreadSafeQueue<ulong>)state;
+            while (true)
+            {
+                if (queue.Count == 0)
+                    Thread.Sleep(3000);
+                yield return queue.Dequeue();
+            }
+        }
+    }
+}
---
+++ QueuesWithYieldReturn/QueuesWithYieldReturn/ThreadSafeQueue.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueuesWithYieldReturn
+{
+    public class ThreadSafeQueue<T>
+    {
+        private Queue<T> queue = new Queue<T>();
+        public int Count
+        {
+            get
+            {
+                lock (queue)
+                {
+                    return queue.Count;
+                }
+            }
+        }
+        public void Enqueue(T value)
+        {
+            lock (queue)
+            {
+                queue.Enqueue(value);
+            }
+        }
+        public T Dequeue()
+        {
+            lock (queue)
+            {
+                return queue.Dequeue();
+            }
+        }
+        public T Peek()
+        {
+            lock (queue)
+            {
+                return queue.Peek();
+            }
+        }
+    }
+}
